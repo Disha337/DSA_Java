8e4c80225e64d070a6f9b0003b519e01052f4c0f
Disha337 <dhopedisha8@gmail.com>
2025-12-24 18:05:05 +0530

    Add Contains Duplicate solution using HashSet

---
+++ Hashing/ContainsDuplicate.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        //It requires too much time so time limit exceeded
+        /*for(int i=0; i<nums.length-1; i++){
+            Arrays.sort(nums);
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+
+        return false;*/
+
+
+        //Using set because set doesn't contain duplicate elements
+
+        HashSet set=new HashSet<>();
+        for(int num : nums){
+            if(!set.add(num)){
+                return true;
+            }
+        }
+
+        return false;
+        
+    }
+}
