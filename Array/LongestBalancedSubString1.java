41d0f93dbde9c89779372bc4b55177a6d5377552
Disha337 <dhopedisha8@gmail.com>
2026-02-13 01:08:14 +0530

    Add Solution Leetcode 3713 Longest Balanced SubString I

---
+++ Array/LongestBalancedSubString1.java
@@ -0,0 +1,30 @@
+//Leetcode 3713. Longest Balanced Substring I
+//Level : Medium
+//Technique Used : Frequency Array + Dynamic Update + Mathematical Check
+//Time Complexity : O(n²)
+//Space Complexity : O(1)
+
+class Solution{
+  public int longestBalanced(String s){
+    int n = s.length();
+    int ans = 1;
+    for(int i=0; i<n; i++){
+      int[] freq = new int[26];
+      int distinct = 0;
+      int maxFreq = 0;
+      for(int j=i; j<n; j++){
+        int idx = s.charAt(j) - 'a';
+        if(freq[idx] == 0){
+          distinct++;
+        }
+        freq[idx]++;
+        maxFreq = Math.max(maxFreq, freq[idx]);
+        int len = j-i+1;
+        if(maxFreq * distinct == len){
+          ans = Math.max(ans,len);
+        }
+      }
+    }
+    return ans;
+  }
+}
