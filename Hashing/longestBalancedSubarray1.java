7db21f2940ce652880f171622c80351241cb7e50
Disha337 <dhopedisha8@gmail.com>
2026-02-12 18:42:03 +0530

    Add Solution Leetcode 3719 Longest Balanced Subarray I

---
+++ Hashing/longestBalancedSubarray1.java
@@ -0,0 +1,30 @@
+//Leetcode 3719. Longest Balanced Subarray I
+//Level : Medium
+//Techique Used : HasSet + Brute Force
+//Time Complexity : O(n2)
+//Space Complexity : O(n)
+
+class Solution{
+  public int longestBalanced(int[] nums){
+    int n = nums.length;
+    int maxLen = 0;
+
+    for(int i = 0; i<n; i++){
+      Set<Integer> evenSet = new HashSet<>();
+      Set<Integer> oddSet = new HashSet<>();
+
+      for(int j=i; j<n; j++){
+        if(nums[j] % 2 == 0){
+          evenSet.add(nums[j]);
+        }
+        else{
+          oddSet.add(nums[j]);
+        }
+        if(evenSet.size() == oddSet.size()){
+          maxLen = Math.max(maxLen, j-i+1);
+        }
+      }
+    }
+    return maxLen;
+  }
+}
